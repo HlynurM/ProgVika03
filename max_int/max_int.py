6d22ed8b831c5a69860cb27f0b254f09fe471b6e
Hlynur Magnus <futomatik@gmail.com>
2018-09-04 09:42:29 +0000

    Setup til ad tengjast git repo

---
+++ max_int/max_int.py
@@ -0,0 +1,14 @@
+# Hlynur Magnus
+
+# get integer numbers
+# check if number is negative
+# if number is negative then go to next stage
+# if still positive then keep allowing user to add integer numbers
+
+
+
+
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
